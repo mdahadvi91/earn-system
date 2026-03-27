0254de9a7e17009fa09c65cbc8ce668f7b0862d6
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-28 03:43:53 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let users = {};
+// static folder (web)
+app.use(express.static(path.join(__dirname, "web")));
 
+// HOME ROUTE FIX 🔥
 app.get("/", (req, res) => {
-  res.send("Server Running ✅");
+  res.sendFile(path.join(__dirname, "web", "index.html"));
 });
 
+// user system
+let users = {};
+
+// get user
 app.get("/user/:id", (req, res) => {
-  const id = req.params.id;
+  let id = req.params.id;
   if (!users[id]) users[id] = { balance: 0 };
   res.json(users[id]);
 });
 
+// reward
 app.post("/reward", (req, res) => {
-  const { id, amount } = req.body;
+  let { id, amount } = req.body;
   if (!users[id]) users[id] = { balance: 0 };
   users[id].balance += amount;
   res.json({ success: true });
 });
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(3000, () => console.log("Server running on 3000"));
