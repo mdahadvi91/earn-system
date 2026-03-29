bd06133f9e15dddeaed143152c43e444c57c669f
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-29 21:21:27 +0400

    Update task.js

--- web/js/task.js
+++ web/js/task.js
@@ -1,20 +1,21 @@
-let tg = window.parent.Telegram.WebApp;
-let user = tg.initDataUnsafe.user.id;
+function watchAd(){
 
-function watch(){
+  // 🔥 Adsterra link
+  window.open("https://www.profitablecpmratenetwork.com/kxq650wd?key=11d1ec5f3d88b1d9689e0547e8b15dd1");
 
-window.open("https://www.profitablecpmratenetwork.com/kxq650wd?key=11d1ec5f3d88b1d9689e0547e8b15dd1");
-
-setTimeout(()=>{
-fetch("/reward",{
-method:"POST",
-headers:{'Content-Type':'application/json'},
-body:JSON.stringify({id:user})
-})
-.then(r=>r.json())
-.then(d=>{
-alert(d.success ? "Earned" : d.error);
-});
-},15000);
-
+  setTimeout(()=>{
+    fetch("/reward",{
+      method:"POST",
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({id:user})
+    })
+    .then(r=>r.json())
+    .then(d=>{
+      if(d.success){
+        alert("Earned "+d.reward.toFixed(4)+" USDT");
+      }else{
+        alert(d.error);
+      }
+    });
+  },15000);
 }
