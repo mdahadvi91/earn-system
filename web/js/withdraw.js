4d21e880a3f6f1030cda67515e96374d38822352
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-29 21:23:22 +0400

    Update withdraw.js

--- web/js/withdraw.js
+++ web/js/withdraw.js
@@ -1,18 +1,15 @@
-let tg = window.parent.Telegram.WebApp;
-let user = tg.initDataUnsafe.user.id;
-
-function wd(){
+function withdraw(){
 fetch("/withdraw",{
 method:"POST",
 headers:{'Content-Type':'application/json'},
 body:JSON.stringify({
-id:user,
-amount:amount.value,
-number:addr.value
+  id:user,
+  amount:document.getElementById("amount").value,
+  address:document.getElementById("addr").value
 })
 })
 .then(r=>r.json())
 .then(d=>{
-alert(d.success?"Done":"Error");
+  alert(d.success ? "Request Sent" : "Error");
 });
 }
