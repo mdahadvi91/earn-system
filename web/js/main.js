cc54f33aee02878c701d6e7671a499d83bbbe5d3
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-29 21:20:23 +0400

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,25 @@
-function go(page){
-document.getElementById("frame").src = "pages/"+page+".html";
+// main.js
+
+let tg = window.Telegram.WebApp;
+let user = tg.initDataUnsafe?.user?.id || "demo";
+
+document.getElementById("uid").innerText = user;
+
+// page switch
+function showPage(page){
+  document.getElementById("content").innerHTML = "Loading...";
+  
+  fetch("pages/" + page + ".html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("content").innerHTML = html;
+
+    // load js for that page
+    let script = document.createElement("script");
+    script.src = "js/" + page + ".js";
+    document.body.appendChild(script);
+  });
 }
+
+// first load
+showPage("task");
