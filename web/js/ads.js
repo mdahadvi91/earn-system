74ddafe029af02bacfdf0d30cfe5d64a5c75b017
mdahadvi91 <mdahadvi91@gmail.com>
2026-03-29 22:03:58 +0400

    Update ads.js

--- web/js/ads.js
+++ web/js/ads.js
@@ -7,27 +7,33 @@ function initTask(){
 function watchAd(){
 
   if(running){
-    alert("⏳ Wait...");
+    alert("Wait...");
     return;
   }
 
   running = true;
 
-  // 🔥 Smartlink
-  window.open("https://www.profitablecpmratenetwork.com/kxq650wd?key=11d1ec5f3d88b1d9689e0547e8b15dd1", "_blank");
+  let device = navigator.userAgent;
+
+  window.open("https://www.profitablecpmratenetwork.com/kxq650wd?key=11d1ec5f3d88b1d9689e0547e8b15dd1");
 
   setTimeout(()=>{
 
     fetch("/api/reward",{
       method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify({id:user})
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        id:user,
+        device:device
+      })
     })
     .then(r=>r.json())
     .then(d=>{
       alert(d.msg);
-      loadUser();
       running = false;
+      loadUser();
     });
 
   },15000);
