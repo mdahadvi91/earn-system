1a5b148cd42ee6653c3f54e081775c82880de1d3
mdahadvi91 <mdahadvi91@gmail.com>
2026-04-01 07:55:03 +0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,17 @@
+// middleware.js
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
+const helmetMiddleware = helmet();
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later."
+});
+
+module.exports = {
+  helmet: helmetMiddleware,
+  apiLimiter,
+  // deviceFraudMiddleware পরে লাগলে যোগ করবেন
+};
